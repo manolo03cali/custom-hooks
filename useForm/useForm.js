f563a9c6adf3978557fd003851535ea8f67fb6fe
manolo03cali <manolo.ing@gmail.com>
2025-05-26 07:38:49 -0500

    Update useForm.js
    Actualización de hook

--- useForm/useForm.js
+++ useForm/useForm.js
@@ -1,34 +1,58 @@
-// Importamos el hook useState desde React para manejar el estado del formulario
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
-// Definimos el custom hook useForm, que recibe como parámetro un objeto inicial para el formulario
-export const useForm = (initialForm = {}) => {
-  // Creamos un estado llamado formState con los valores iniciales del formulario
-  const [formState, setformState] = useState(initialForm);
+export const useForm = (initialForm = {}, formValidations = {}) => {
+  const [formState, setFormState] = useState(initialForm);
+  const [formValidation, setFormValidation] = useState({});
 
-  // Esta función se encarga de manejar los cambios en los inputs del formulario
+  useEffect(() => {
+    createValidators();
+  }, [formState]);
+
+  useEffect(() => {
+    setFormState(initialForm);
+  }, [initialForm]);
+
+  const isFormValid = useMemo(() => {
+    for (const formValue of Object.keys(formValidation)) {
+      if (formValidation[formValue] !== null) return false;
+    }
+
+    return true;
+  }, [formValidation]);
+
   const onInputChange = ({ target }) => {
     const { name, value } = target;
-
-    // Actualizamos el estado del formulario usando el nombre del input como clave
-    setformState({
-      ...formState, // Mantenemos los demás valores del formulario
-      [name]: value, // Actualizamos solo el campo que cambió
+    setFormState({
+      ...formState,
+      [name]: value,
     });
   };
 
   const onResetForm = () => {
-    setformState(initialForm);
+    setFormState(initialForm);
   };
 
-  // Retornamos:
-  // - Todas las propiedades individuales del formulario (spread de formState)
-  // - El estado completo del formulario (formState)
-  // - La función para manejar cambios en los inputs (onInputChange)
+  const createValidators = () => {
+    const formCheckedValues = {};
+
+    for (const formField of Object.keys(formValidations)) {
+      const [fn, errorMessage] = formValidations[formField];
+
+      formCheckedValues[`${formField}Valid`] = fn(formState[formField])
+        ? null
+        : errorMessage;
+    }
+
+    setFormValidation(formCheckedValues);
+  };
+
   return {
-    ...formState, // Permite acceder a los valores del formulario directamente (ej: username, email, etc.)
-    formState, // Permite acceder al estado completo del formulario si se necesita
-    onInputChange, // Se utiliza en los inputs para manejar los cambios
+    ...formState,
+    formState,
+    onInputChange,
     onResetForm,
+
+    ...formValidation,
+    isFormValid,
   };
 };
